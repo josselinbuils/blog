3102cb8c10d9dd8d708bc07f3c077298aaf95633
Josselin BUILS <josselin.buils@gmail.com>
2020-11-08 10:33:01 +0100

    Simplifies hot reload to prevent client issues

--- scripts/utils/HotReload.tsx
+++ scripts/utils/HotReload.tsx
@@ -3,26 +3,11 @@ import React, { FC } from 'react';
 export const HotReload: FC = () => {
   function watchDevServer(): void {
     const socket = new WebSocket(`ws://localhost:3001`);
-    let connectionWasLost = false;
 
-    function reload() {
-      console.log('Reload...');
-      window.location.reload();
-    }
-
-    socket.onclose = () => {
-      connectionWasLost = true;
-      setTimeout(watchDevServer, 3000);
-    };
-    socket.onerror = socket.onclose as any;
     socket.onmessage = ({ data }) => {
       if (data === 'reload') {
-        reload();
-      }
-    };
-    socket.onopen = () => {
-      if (connectionWasLost) {
-        reload();
+        console.log('Reload...');
+        window.location.reload();
       }
     };
   }
